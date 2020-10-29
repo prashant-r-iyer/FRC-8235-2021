4cabe7d2625ddd69e227d7beae4816b13d71e559
Prashant <prashantriyer@hotmail.com>
2020-10-29 19:02:54 +0800

    changed object type of primayController

--- FRC 8235 2021 code/src/main/java/frc/robot/RobotContainer.java
+++ FRC 8235 2021 code/src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,7 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -20,11 +18,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   
-
-
-
-  public static Object primaryController;
-
+  public static XboxController primaryController;
 
 /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
