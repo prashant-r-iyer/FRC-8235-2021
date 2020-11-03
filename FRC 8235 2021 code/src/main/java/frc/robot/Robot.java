440bc3919a5336d9065f1e36d7dd8e7425a33b3a
Prashant <prashantriyer@hotmail.com>
2020-11-03 19:25:11 +0800

    updated testInit() with testSpeed

--- FRC 8235 2021 code/src/main/java/frc/robot/Robot.java
+++ FRC 8235 2021 code/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.cDrive;
 
 
 /**
@@ -102,6 +103,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testInit() {
+    cDrive.straightSpeed = Constants.testSpeed;
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
