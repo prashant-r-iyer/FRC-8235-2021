f26ce681cfac25a18d3c4befb34d88f384c7a9e6
Madhav <madhavlodha2503@gmail.com>
2020-11-08 23:23:26 +0800

    There is a error in sIntake

--- FRC 8235 2021 code/src/main/java/frc/robot/subsystems/sIntake.java
+++ FRC 8235 2021 code/src/main/java/frc/robot/subsystems/sIntake.java
@@ -10,6 +10,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
