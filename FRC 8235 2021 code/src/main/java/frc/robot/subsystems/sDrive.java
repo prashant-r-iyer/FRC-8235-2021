4b83fe5c0f57e051bcb9dda8b77a5f737884b919
Jaiz <jaizepic@gmail.com>
2021-01-28 20:38:44 +0800

    Literally pressed enter (again)

--- FRC 8235 2021 code/src/main/java/frc/robot/subsystems/sDrive.java
+++ FRC 8235 2021 code/src/main/java/frc/robot/subsystems/sDrive.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.commands.cDrive;
 
+
 public class sDrive extends SubsystemBase {
 
   public static WPI_VictorSPX leftFrontMotor;
