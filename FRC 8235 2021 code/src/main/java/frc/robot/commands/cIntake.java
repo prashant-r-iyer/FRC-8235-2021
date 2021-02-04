29bb167bca08f4abff13d7d1910b7eab24b4dfd5
Prashant <prashantriyer@hotmail.com>
2021-02-05 00:40:14 +0800

    changes

--- FRC 8235 2021 code/src/main/java/frc/robot/commands/cIntake.java
+++ FRC 8235 2021 code/src/main/java/frc/robot/commands/cIntake.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.sIntake;
 //Madhav spx
@@ -60,7 +59,7 @@ public class cIntake extends CommandBase {
       intakeSubsystem.stop();
     }
   }
-//Fat motor = new Madhavspx(fatport)
+//Fat motor = new Madhavspx(fatport);
   // Called once the command ends or is interrupted.
   //@Override
   public void end(boolean interrupted) {
