1ccca8859377a9f646fd9de0b6338d63237b11b2
Jaiz <jaizepic@gmail.com>
2021-02-04 23:42:23 +0800

    fatty.

--- FRC 8235 2021 code/src/main/java/frc/robot/commands/cIntake.java
+++ FRC 8235 2021 code/src/main/java/frc/robot/commands/cIntake.java
@@ -29,18 +29,18 @@ public class cIntake extends CommandBase {
   }
 
   // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (RobotContainer.primaryController.getXButtonPressed()) {
-      intakeSubsystem.hatchOpen();
-    }
-    if (RobotContainer.primaryController.getBButtonPressed()) {
-      intakeSubsystem.hatchClosed();
-    }
-  }
+  //@Override
+  //public void execute() {
+    //if (RobotContainer.primaryController.getXButtonPressed()) {
+      //intakeSubsystem.hatchOpen();
+    //}
+    //if (RobotContainer.primaryController.getBButtonPressed()) {
+      //intakeSubsystem.hatchClosed();
+    //}
+  //}
 
   // Called once the command ends or is interrupted.
-  @Override
+  //@Override
   public void end(boolean interrupted) {
     //intakeSubsystem.hatchClosed();
   }
